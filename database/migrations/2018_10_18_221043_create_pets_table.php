e52781eb4131c2d9f109bcd513030a7bf83b032b
Anderson Rodrigues Palmeira <andersonarp2810@gmail.com>
2018-10-30 19:09:01 -0300

    make auth

--- database/migrations/2018_10_18_221043_create_pets_table.php
+++ database/migrations/2018_10_18_221043_create_pets_table.php
@@ -26,7 +26,7 @@ class CreatePetsTable extends Migration
 
             $table->timestampsTz();
 
-            $table->foreing('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 

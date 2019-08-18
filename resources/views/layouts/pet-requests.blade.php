2bed82340596e4e8987e92e579b7006be328bfbd
beatrizf13 <azz.felix7@gmail.com>
2019-08-17 21:17:02 -0300

    responsividade solicitações

--- resources/views/layouts/pet-requests.blade.php
+++ resources/views/layouts/pet-requests.blade.php
@@ -1,86 +1,83 @@
-<div class="modal fade" id="{{'modal-requests'}}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog cascading-modal modal-avatar .modal-content1" role="document">
+<div class="modal row" id="{{'modal-requests'}}" tabindex="-1" role="dialog" aria-hidden="true">
+    @csrf
+    <div class="modal-dialog modal-lg modal-dialog-centered pl-5" role="document">
         <div class="modal-content">
-            <div class="modal-header">
-                <h1 class="card-title">Solicitações enviadas por você</h1>
+            <div class="modal-header text-center">
+                <h2 class="mt-3">Solicitações enviadas por você</h2>
             </div>
 
-            <div class="modal-body text-center mb-1">
+            <div class="modal-body mb-1">
                 <div class="row wow fadeIn">
                     @foreach ($solicitations as $solicitation)
-                    @foreach(Auth::User()->pet as $pet)
-                    @if($solicitation->requesters_pet_id == $pet->id)
-                    <div class="col-lg-4 col-md-4 mb-4">
-                        <div class="card card-cascade wider h-75">
-                            @include('layouts.pet-card-image', ['pet' => $solicitation->requesters_pet])
-                            <div class="card-body card-body-cascade text-center" style="padding: 10px 0px 0px !important">
-                                <h5 class="card-title" style="margin-bottom: 6px !important"><strong>{{ $pet->name }}</strong></h5>
-                                @if($solicitation->status == 'aceito')
-                                <div class="row">
-                                    <div class="col-md-12 text-center">
-                                        <button type="button" class="btn btn-info" disabled>
-                                            Aceito
-                                            <i class="fa fa-check animated rotateIn"></i>
-                                        </button>
+                        @foreach(Auth::User()->pet as $pet)
+                        @if($solicitation->requesters_pet_id == $pet->id)
+                        <div class="col-md-4 mb-4">
+                            <div class="card card-cascade wider h-75">
+                                @include('layouts.pet-card-image', ['pet' => $solicitation->requesters_pet])
+                                <div class="card-body card-body-cascade text-center" style="padding: 10px 0px 0px !important">
+                                    <h5 class="card-title" style="margin-bottom: 6px !important"><strong>{{ $pet->name }}</strong></h5>
+                                    @if($solicitation->status == 'aceito')
+                                    <div class="row">
+                                        <div class="col-md-12 text-center">
+                                            <button type="button" class="btn btn-success" disabled>
+                                                Aceito <i class="fa fa-check"></i>
+                                            </button>
+                                        </div>
                                     </div>
-                                </div>
-                                @else
-                                <div class="row">
-                                    <div class="col-md-12 text-center">
-                                        <button type="button" class="btn btn-info" disabled>
-                                            Pendente
-                                            <i class="fa fa-hourglass-half animated rotateIn"></i>
-                                        </button>
+                                    @else
+                                    <div class="row">
+                                        <div class="col-md-12 text-center">
+                                            <button type="button" class="btn btn-light" disabled>
+                                                Pendente <i class="fa fa-hourglass-half"></i>
+                                            </button>
+                                        </div>
                                     </div>
+                                    @endif
                                 </div>
-                                @endif
                             </div>
                         </div>
-                    </div>
-                    @endif
-                    @endforeach
+                        @endif
+                        @endforeach
 
-                    @foreach($pets as $pet)
-                    @if($solicitation->requesteds_pet_id == $pet->id)
-                    <div class="col-lg-4 col-md-4 mb-4">
+                        @foreach($pets as $pet)
+                        @if($solicitation->requesteds_pet_id == $pet->id)
+                        <div class="col-md-4 mb-4">
+                            <div class="card card-cascade wider h-75">
+                                @include('layouts.pet-card-image', ['pet' => $solicitation->requested_pet])
+                                <div class="card-body card-body-cascade text-center" style="padding: 10px 0px 0px !important">
+                                    <h5 class="card-title" style="margin-bottom: 6px !important"><strong>{{ $pet->name }}</strong></h5>
+                                    @if($solicitation->status == 'aceito')
+                                    <div class="row">
+                                        <div class="col-md-12 text-center">
+                                            <button type="submit" class="btn btn-success" disabled>
+                                                Aceito <i class="fas fa-check"></i>
+                                            </button>
 
-                        <div class="card card-cascade wider h-75">
-                            @include('layouts.pet-card-image', ['pet' => $solicitation->requested_pet])
-                            <div class="card-body card-body-cascade text-center" style="padding: 10px 0px 0px !important">
-                                <h5 class="card-title" style="margin-bottom: 6px !important"><strong>{{ $pet->name }}</strong></h5>
-                                @if($solicitation->status == 'aceito')
-                                <div class="row">
-                                    <div class="col-md-12 text-center">
-                                        <button type="submit" class="btn btn-info" disabled>
-                                            Aceito
-                                            <i class="fa fa-check ml- 1 animated rotateIn"></i>
-                                        </button>
-
+                                        </div>
                                     </div>
+                                    @else
+                                    <form method="POST" action="{{ route('solicitation.update', ['solicitation' => $solicitation]) }}" enctype="multipart/form-data">
+                                        @csrf
+                                        <input type="hidden" name="status" value="aceito">
+                                        <button type="submit" class="btn btn-sucess">
+                                            Aceitar
+                                            <i class="fa fa-check ml-1 animated rotateIn"></i>
+                                        </button>
+                                    </form>
+                                    <form method="POST" action="{{ route('solicitation.update', ['solicitation' => $solicitation]) }}" enctype="multipart/form-data">
+                                        @csrf
+                                        <input type="hidden" name="status" value="recusado">
+                                        <button type="submit" class="btn btn-danger">
+                                            Recusar
+                                            <i class="fa fa-times ml-1 animated rotateIn"></i>
+                                        </button>
+                                    </form>
+                                    @endif
                                 </div>
-                                @else
-                                <form method="POST" action="{{ route('solicitation.update', ['solicitation' => $solicitation]) }}" enctype="multipart/form-data">
-                                    @csrf
-                                    <input type="hidden" name="status" value="aceito">
-                                    <button type="submit" class="btn btn-info">
-                                        Aceitar
-                                        <i class="fa fa-check ml-1 animated rotateIn"></i>
-                                    </button>
-                                </form>
-                                <form method="POST" action="{{ route('solicitation.update', ['solicitation' => $solicitation]) }}" enctype="multipart/form-data">
-                                    @csrf
-                                    <input type="hidden" name="status" value="recusado">
-                                    <button type="submit" class="btn btn-danger btn-sm">
-                                        Recusar
-                                        <i class="fa fa-times ml-1 animated rotateIn"></i>
-                                    </button>
-                                </form>
-                                @endif
                             </div>
                         </div>
-                    </div>
-                    @endif
-                    @endforeach
+                        @endif
+                        @endforeach
                     @endforeach
                 </div>
             </div>
